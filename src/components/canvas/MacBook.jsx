f240e57fc0cf8c613194ecb5d448a0da6d082240
mvoemel <michael@voemel.org>
2023-11-10 11:36:01 +0100

    deactivated autorotate on MacBookCanvas

--- src/components/canvas/MacBook.jsx
+++ src/components/canvas/MacBook.jsx
@@ -69,8 +69,8 @@ const MacBookCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          autoRotate
-          autoRotateSpeed={0.4}
+          // autoRotate
+          // autoRotateSpeed={0.4}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
