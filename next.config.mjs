117507a9950144b008018bd9e362120e7aa0bfe4
mvoemel <michael@voemel.org>
2024-06-15 14:22:38 +0200

    MAINT: added comment to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // TODO: Configure Next.js build options
+  // TODO: Configure Next.js build options for only client-side rendering. Change in future when server-side rendering is implemented.
   output: "export",
   typescript: {
     ignoreBuildErrors: true,
